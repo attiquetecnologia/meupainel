c1b1b1f26b5da3de56d6bc374812311a7210d574
Rodrigo Attique Santana <attiquetecnologia@gmail.com>
2016-12-19 14:51:03 -0200

    Add files via upload

---
+++ categoria/categorias.php
@@ -0,0 +1,27 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<div class="ttl-conteudo">
+    Categorias
+</div>
+<a href="index.php?pagina=addcategoria">New</a>
+<table class="w3-table-all">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>NOME</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php
+require_once '/lib/entidade/categoria.php';
+$categoria = new Categoria();
+foreach ($categoria->all() as $key) {
+    echo "<tr>";
+    echo "<td>".$key->getId()."</td>";
+    echo '<td><a href="index.php?pagina=editcategoria&id='.$key->getId().'">'.$key->getNome()."</a></td>";
+    echo "</tr>";
+}
+?>      
+</tbody>
+</table>
+<a href="index.php?pagina=addcategoria">New</a>
